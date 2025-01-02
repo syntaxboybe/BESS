cec05ff882bd5f09cacb61c9e988ed3fe50e78f1
Syntaxboybe <beboymegalbioeyana@gmail.com>
2025-01-02 20:19:25 +0800

    fix(routes): for default root in the system url

--- apps/UserPortal/urls.py
+++ apps/UserPortal/urls.py
@@ -6,33 +6,37 @@ from .views import *
 from .forms import CaptchaPasswordChangeForm
 
 urlpatterns = [
-    path ('Home/', home, name='home'),
-    path ('About/', about, name='about'),
-    path ('Contact/', contact, name='contact'),
-    path ('Announcement/', announce, name='announce'),
-    path ('ServicePortal/', servicesPortal, name='service_portal'),
-    path ('Profile/', profile, name='profile'),
-    path ('Privacy/', privacy, name='privacy'),
-    path ('Change Email/', changeEmail, name='changeEmail'),
-    path ('Change Username/', changeUsername, name='changeUsername'),
-    path ('User Logout/', userLogout, name='userLogout'),
-    path ('BarangayClearance/', barangay_clearance, name='barangay_clearance'),
-    path ('indigency/', indigency, name='indigency'),
-    path ('BuildingPermit/', BuildingPermit, name='BuildingPermit'),
-    path ('BusinessPermit/', BusinessPermit, name='BusinessPermit'),
-    path ('ResidencyCertificate/', ResidencyCertificate, name='ResidencyCertificate'),
-    path ('document-status/', document_status, name='document-status'),
-
-
-
-    path('password_change', 
-    auth_views.PasswordChangeView.as_view(template_name='ChangePassword/password_change.html', form_class=CaptchaPasswordChangeForm), 
-    name='password_change'),
-
-    path('password_change_done', 
-    auth_views.PasswordChangeDoneView.as_view(template_name='ChangePassword/password_change_done.html'), 
-    name='password_change_done'),
-    
+    path("", home, name="home"),
+    path("About/", about, name="about"),
+    path("Contact/", contact, name="contact"),
+    path("Announcement/", announce, name="announce"),
+    path("ServicePortal/", servicesPortal, name="service_portal"),
+    path("Profile/", profile, name="profile"),
+    path("Privacy/", privacy, name="privacy"),
+    path("Change Email/", changeEmail, name="changeEmail"),
+    path("Change Username/", changeUsername, name="changeUsername"),
+    path("User Logout/", userLogout, name="userLogout"),
+    path("BarangayClearance/", barangay_clearance, name="barangay_clearance"),
+    path("indigency/", indigency, name="indigency"),
+    path("BuildingPermit/", BuildingPermit, name="BuildingPermit"),
+    path("BusinessPermit/", BusinessPermit, name="BusinessPermit"),
+    path("ResidencyCertificate/", ResidencyCertificate, name="ResidencyCertificate"),
+    path("document-status/", document_status, name="document-status"),
+    path(
+        "password_change",
+        auth_views.PasswordChangeView.as_view(
+            template_name="ChangePassword/password_change.html",
+            form_class=CaptchaPasswordChangeForm,
+        ),
+        name="password_change",
+    ),
+    path(
+        "password_change_done",
+        auth_views.PasswordChangeDoneView.as_view(
+            template_name="ChangePassword/password_change_done.html"
+        ),
+        name="password_change_done",
+    ),
 ]
 
 # urlpatterns = [
@@ -44,3 +48,4 @@ urlpatterns = [
 
 
 # ]
+
